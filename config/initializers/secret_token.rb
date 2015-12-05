2eca0d70a7aae09386825d3284506534410ebb22
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-12-05 15:54:26 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techhack20152::Application.config.secret_token = '3a2087937fb0b2086359248abb909425f9b329491620ab4cc1dcae5a57f7dbe14bbf8f5de85e9a307b748c96797ca20f48dbad2c1d1db6c42b6aa182990211d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Techhack20152::Application.config.session_store :cookie_store, key: '_techhack_20152_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Techhack20152::Application.config.session_store :active_record_store
